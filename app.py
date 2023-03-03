13fb8a8b3c2e8968e36de14e2379234a4b055a08
Björn Plüster <bjoern.pl@outlook.de>
2023-03-03 14:09:55 +0100

    Added examples from main repo

--- app.py
+++ app.py
@@ -19,9 +19,28 @@ def generate_text(text):
 
 
 examples = [
-    ["The capital of Germany is the city of"],
-    ["Here is my sonnet in the style of Shakespeare about an artificial intelligence:"]
-]
+    # For these prompts, the expected answer is the natural continuation of the prompt
+    "I believe the meaning of life is",
+    "Simply put, the theory of relativity states that ",
+    "Building a website can be done in 10 simple steps:\n",
+    # Few shot prompts: https://huggingface.co/blog/few-shot-learning-gpt-neo-and-inference-api
+    """Tweet: "I hate it when my phone battery dies."
+Sentiment: Negative
+###
+Tweet: "My day has been 👍"
+Sentiment: Positive
+###
+Tweet: "This is the link to the article"
+Sentiment: Neutral
+###
+Tweet: "This new music video was incredibile"
+Sentiment:""",
+    """Translate English to French:
+sea otter => loutre de mer
+peppermint => menthe poivrée
+plush girafe => girafe peluche
+cheese =>""",
+    ]
 
 gr.Interface(
     generate_text,
